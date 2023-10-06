bb5f9c909b60c62920ad7407fdb07cbfa29cc521
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2023-10-05 20:43:14 -0400

    feat: Add category routes

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import "dotenv/config";
+import indexRouter from "./routes/index.js";
+import catalogRouter from "./routes/catalog.js";
 
 const port = process.env.PORT || 5000;
 const app = express();
 
-import indexRouter from './routes/index.js';
-
 app.use("/", indexRouter);
+app.use("/catalog", catalogRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- routes/catalog.js
+++ routes/catalog.js
@@ -1,13 +1,57 @@
 import express from 'express';
-const router = express.Router();
+const routerCategory = express.Router();
 
 /// Category Routes ///
 
-// GET request to the homepage
-router.get('/', (req, res) => {
-  res.send('GET request to the homepage')
-})
+// GET request to the homepage (all categories)
+routerCategory.get("/", (req, res) => {
+  res.send("GET request to the homepage");
+});
+
+// GET request to get a category
+routerCategory.get("/category", (req, res) => {
+  res.send("GET request to the create category page, NOT IMPLEMENTED!");
+});
+
+// POST request to create a category
+routerCategory.post("/category", (req, res) => {
+  res.send("POST request to the create category page, NOT IMPLEMENTED!");
+});
+
+// GET request to update a category
+routerCategory.put("/category", (req, res) => {
+  res.send("PUT request to the create category page, NOT IMPLEMENTED!");
+});
+
+// GET request to delete a category
+routerCategory.delete("/category", (req, res) => {
+  res.send("POST request to the create category page, NOT IMPLEMENTED!");
+});
+
+// POST request to delete a category
+
+// GET request to update a category
+
+// POST request to update a category
+
+// GET request for one category
 
 /// Item Routes ///
 
-export default router;
+// GET request for all items
+
+// GET request for one item
+
+// GET request to create an item
+
+// POST request to create an item
+
+// GET request to delete an item
+
+// POST request to delete an item
+
+// GET request to update an item
+
+// POST request to update an item
+
+export default routerCategory;

4e714feb9b67def38970e0e15406f692d5a33f0d
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2023-10-10 21:45:28 -0400

    feat: Add index page
    Add index page to the app.
    Show all categories on the index page.'

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ import "dotenv/config";
 import indexRouter from "./routes/index.js";
 import catalogRouter from "./routes/catalog.js";
 import main from './db/mongoose.js';
+import { fileURLToPath } from "url";
 
 const port = process.env.PORT || 5000;
 const app = express();
 
+
+const viewsPath = fileURLToPath(new URL("views", import.meta.url));
+const staticsPath = fileURLToPath(new URL("public", import.meta.url));
+
+app.set("views", viewsPath);
+app.set("view engine", "ejs");
+
 main().catch((err) => console.log(err));
 
 app.use(express.json());
+app.use(express.static(staticsPath));
 
 app.use("/", indexRouter);
 app.use("/catalog", catalogRouter);
--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,9 +3,9 @@ import Category from "../models/categories.js";
 // Display all categories
 const categoryList = async(req, res, next) => {
     try {
-        const allCategories = await Category.find().sort({ name: 1 });
-        res.status(200).json({
-            title: 'Categories',
+        const allCategories = await Category.find().sort({ name: 1 }).exec();
+        res.render('index', {
+            title: 'Inventory Application',
             all_categories: allCategories,
         });
     } catch (error) {

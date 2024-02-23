9143bef52456f8ecc9c485768eab0b3ef45d2eee
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2024-02-23 01:41:00 -0500

    workflow: fix cd workflow for http

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,7 +4,7 @@
 echo $(aws ssm get-parameter --name "docker-creds-token" --query "Parameter.Value" --output text) | docker login --username $(aws ssm get-parameter --name "docker-creds-username" --query "Parameter.Value" --output text) --password-stdin
 
 # Get Docker Image from repo
-docker pull "$(aws ssm get-parameter --name "docker-image-name-inventory-app" --query "Parameter.Value" --output text)amd-arm-https"
+docker pull "$(aws ssm get-parameter --name "docker-image-name-inventory-app" --query "Parameter.Value" --output text)amd-arm"
 
 # Run the Code Deploy Agent
 sudo systemctl start codedeploy-agent

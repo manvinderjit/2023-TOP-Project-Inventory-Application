7318022fee436415d547bff1956906a87837e235
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2024-09-09 18:32:18 -0400

    setup: Setup config for ts and jest
    Setup separate config for build command.
    Setup config for using jest and typescript with import.meta.

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,16 @@
 import type { Config } from "jest";
+import { createDefaultEsmPreset } from 'ts-jest'
 
-const config: Config = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    roots: ['<rootDir>/__tests__'],
-    maxWorkers: 1,
-    detectOpenHandles: true,
+const jestConfig: Config = {
+        preset: 'ts-jest',
+        testEnvironment: 'node',
+        roots: ['<rootDir>/__tests__'],
+        maxWorkers: 1,
+        detectOpenHandles: true,
+        displayName: 'type-module',
+    ...createDefaultEsmPreset({
+        tsconfig: 'tsconfig-esm.json',
+    }),
 };
 
-export default config;
+export default jestConfig;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express, { Express, Request, Response } from 'express';
 import 'dotenv/config';
 import { Server } from 'http';
+import { fileURLToPath } from 'url';
 
 const port: string | 5000 = process.env.PORT || 5000;
 export const app: Express = express();
 
+const viewsPath = fileURLToPath(new URL('views', import.meta.url));
+const staticsPath = fileURLToPath(new URL('.', import.meta.url));
+
+app.set('views', viewsPath);
+app.set('view engine', 'ejs');
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello Hi!');

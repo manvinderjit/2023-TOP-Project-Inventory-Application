4e41ced7a7db668594aa99456bf576fd38027a1d
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2024-09-15 16:10:36 -0400

    feat: Add pagination to products api
    Add pagination feature for products api.
    Add validation for page query parameter.

--- src/api/controllers/products.controllers.ts
+++ src/api/controllers/products.controllers.ts
@@ -2,7 +2,13 @@ import { NextFunction, Request, Response } from 'express';
 import { fetchProducts } from '../services/products.services.js';
 
 const getProducts = async(req: Request, res: Response) => {
-    const products = await fetchProducts(1,3);
+    let page = 0;
+    
+    if(Array.isArray(req.query.page) && typeof Number(req.query.page[0]) === 'number') {
+        page = Number(req.query.page[0]);
+    } else if (typeof Number(req.query.page) === 'number') page = Number(req.query.page);
+    
+    const products = await fetchProducts(Number(page), 3);
     res.json(products);
 };
 
--- src/api/services/products.services.ts
+++ src/api/services/products.services.ts
@@ -1,42 +1,57 @@
 import Product from "../../app/models/productModel.js";
 import Category from "../../app/models/categoryModel.js";
 
-const fetchProducts = async(page:number = 1, limit:number = 9) => {
+const fetchProducts = async(page:number | null = 0, perPageLimit:number = 6) => {
     
+    // Flag to check if there is a next page
+    let hasNextPage = false; 
+
     try {
         const allProducts = await Product.find()
-            .limit(limit)
-            // .select(
-            //     {
-            //         name: 1,
-            //         description: 1,
-            //         imageFilename: 1,
-            //         price: 1,
-            //         stock: 1,
-            //         // _id: 0,
-            //     },                
-            // )
-            .populate({ path: 'category', select: 'name ', model: Category })
+            .select({
+                name: 1,
+                description: 1,
+                imageFilename: 1,
+                price: 1,
+                stock: 1,
+                _id: 0,
+            })
             .sort({ name: 1 })
+            .limit(Math.round(Number(perPageLimit)) + 1)
+            .skip(Math.round(Number(page)) * Math.round(Number(perPageLimit)))
+            .populate({ path: 'category', select: 'name ', model: Category })
             .exec();
-
+        
+        // If returned result length is greater than page limit => there is a next page (more results)
+        if(allProducts.length > perPageLimit) {
+            hasNextPage = true;
+            // Remove the unrequired last result
+            allProducts.pop();
+        } 
+        
+        // If no products
         if (!allProducts || allProducts.length === 0) {
             return {
                 error: 'No Products to Show!',
-                products: null,
+                productList: null,
+                hasNextPage,
             };
         }
+        // If products
         else {
             return {
                 error: null,
-                products: allProducts,
-            }
+                productList: allProducts,
+                hasNextPage,
+            };
         }
-    } catch (error) {
+    } catch (error) { // if Error
         console.error(error);
         return {
-            error, products: null
-        }
+            error,
+            productList: null,
+            hasNextPage,
+        };
         
     }
 };

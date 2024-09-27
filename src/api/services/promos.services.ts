84ab2aae032baf4fc6d1ee584a75b029305f7dc5
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2024-09-27 18:11:04 -0400

    cleanup: Seggregate fetch promos api
    Seggregate getCarouselPromos api into controller and services.

--- src/api/services/promos.services.ts
+++ src/api/services/promos.services.ts
@@ -1,31 +1,15 @@
 import Promo from '../../models/promoModel.js';
-import { Request, Response, NextFunction } from 'express';
 
-const fetchCarouselPromos = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const carouselPromos = await Promo.find({
-            category: 'Carousel',
-            status: 'Active',
-        })
-        .sort({ name: 1 })
-        .select({ _id: 0, name: 1, caption: 1, category: 1, imageUrl: 1 })
-        .exec();
+const fetchCarouselPromos = async () => {
+    const carouselPromos = await Promo.find({
+        category: 'Carousel',
+        status: 'Active',
+    })
+    .sort({ name: 1 })
+    .select({ _id: 0, name: 1, caption: 1, category: 1, imageUrl: 1 })
+    .exec();
 
-        if (!carouselPromos || carouselPromos.length === 0) {
-            res.status(400).send({
-                message: 'No Promos to Show!',
-            });
-        } else {
-            res.status(200).send({
-                carouselPromos,
-            });
-        }
-    } catch (error) {
-        res.status(400).send({
-            error: 'Something went wrong!',
-        });
-        next(error);
-    }
+    return carouselPromos;
 };
 
 export { fetchCarouselPromos };
--- src/global.middleware/errorHandler.mw.ts
+++ src/global.middleware/errorHandler.mw.ts
@@ -2,8 +2,8 @@ import { Request, Response, NextFunction } from "express";
 
 //Global error handler
 const errorHandler = (err:any, req: Request, res:Response , next: NextFunction): void => {
-    console.error(err.stack);
-    res.status(500).send(err.message);
+    console.error(err.stack);    
+    res.status(500).send(err?.message || 'Something went wrong');
     next();
 };
 

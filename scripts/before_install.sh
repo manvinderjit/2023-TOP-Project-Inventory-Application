f760968c00ee8c06ba6a1864b53b49d041835c8b
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2024-11-07 17:49:21 -0500

    fix: Fix container to include build command

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,7 +4,8 @@
 echo $(aws ssm get-parameter --name "docker-creds-token" --query "Parameter.Value" --output text) | docker login --username $(aws ssm get-parameter --name "docker-creds-username" --query "Parameter.Value" --output text) --password-stdin
 
 # Get Docker Image from repo
-docker pull "$(aws ssm get-parameter --name "docker-image-name-inventory-app" --query "Parameter.Value" --output text)amd-arm"
+# docker pull "$(aws ssm get-parameter --name "docker-image-name-inventory-app" --query "Parameter.Value" --output text)amd-arm"
+docker pull "$(aws ssm get-parameter --name "docker-image-name-inventory-app" --query "Parameter.Value" --output text)amd-arm-v2"
 
 # Run the Code Deploy Agent
 sudo systemctl start codedeploy-agent
